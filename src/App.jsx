63174beb1028a418bc5e96f749fc40dbd0d9b64e
satvikbhat123 <satvik.22cs139@sode-edu.in>
2025-02-24 12:37:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-=======
-import './App.css'
->>>>>>> 9d12b1cb9d1e8dcfd66c306df560240fdd198d24
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +9,6 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <div>
         <h1>counter app</h1>
         <h2>counter value:{count}</h2>
@@ -25,14 +20,6 @@ function App() {
       <button onClick={increase}>increase</button>
       <button onClick={decrease}>decrease</button>
       <button onClick={reset}>reset</button>
-=======
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
->>>>>>> 9d12b1cb9d1e8dcfd66c306df560240fdd198d24
     </>
   )
 }
